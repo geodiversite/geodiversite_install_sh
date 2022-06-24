c08605ad942d40272db01137d3c30b4723529747
brunob <bruno@eliaz.fr>
2022-06-24 12:24:55 +0200

    update du script pour geodiv v3
    ref https://github.com/geodiversite/geodiversite/issues/27

--- install_geodiversite.sh
+++ install_geodiversite.sh
@@ -9,12 +9,10 @@ cd plugins-dist
 git clone -b v2 https://git.spip.net/spip-contrib-extensions/centre_image
 git clone https://git.spip.net/spip-contrib-extensions/compositions
 git clone https://git.spip.net/spip-contrib-extensions/date_inscription
-git clone https://git.spip.net/spip-contrib-extensions/diogene
-git clone -b v1.3 https://git.spip.net/spip-contrib-extensions/emballe_medias
+git clone -b 1.0 https://git.spip.net/spip/bigup
 git clone -b v4 https://git.spip.net/spip-contrib-extensions/facteur
 git clone https://github.com/geodiversite/geodiversite
 git clone https://git.spip.net/spip-contrib-extensions/gis
-git clone https://git.spip.net/spip-contrib-extensions/jquery_file_upload
 git clone https://git.spip.net/spip-contrib-extensions/legendes
 git clone https://git.spip.net/spip-contrib-extensions/menus
 git clone https://git.spip.net/spip-contrib-extensions/mesfavoris
@@ -26,7 +24,6 @@ git clone https://git.spip.net/spip-contrib-extensions/pages
 git clone https://git.spip.net/spip-contrib-extensions/palette
 git clone -b v3 https://git.spip.net/spip-contrib-extensions/polyhierarchie
 git clone -b v3 https://git.spip.net/spip-contrib-extensions/saisies
-git clone https://git.spip.net/spip-contrib-extensions/emballe_medias_swfupload
 git clone https://git.spip.net/spip-contrib-extensions/z-core
 git clone -b v3 https://git.spip.net/spip-contrib-extensions/spip-bonux
 
@@ -41,9 +38,6 @@ git clone https://git.spip.net/spip-contrib-extensions/champs_extras_interface
 git clone https://git.spip.net/spip-contrib-extensions/crayons
 git clone https://git.spip.net/spip-contrib-extensions/criteres_suivant_precedent
 git clone https://git.spip.net/spip-contrib-extensions/crud
-git clone https://git.spip.net/spip-contrib-extensions/diogene_geo
-git clone https://git.spip.net/spip-contrib-extensions/diogene_licence
-git clone https://git.spip.net/spip-contrib-extensions/diogene_mots
 git clone https://git.spip.net/spip-contrib-extensions/embed_code
 git clone https://git.spip.net/spip-contrib-extensions/fulltext
 git clone https://github.com/geodiversite/geodiversite_albums
@@ -53,7 +47,6 @@ git clone https://git.spip.net/spip-contrib-extensions/gravatar
 git clone https://git.spip.net/spip-contrib-extensions/licence
 git clone https://git.spip.net/spip-contrib-extensions/mailshot
 git clone https://git.spip.net/spip-contrib-extensions/mailsubscribers
-git clone https://git.spip.net/spip-contrib-extensions/emballe_medias_collections
 git clone https://git.spip.net/spip-contrib-extensions/memoization
 git clone https://git.spip.net/spip-contrib-extensions/minibando
 git clone https://git.spip.net/spip-contrib-extensions/newsletters
